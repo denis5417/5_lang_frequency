7aa42d9b325971ce2532c8cade06784224843062
denis5417 <denisz.pol@yandex.ru>
2017-07-19 12:17:21 +0300

    Добавлена проверка на существование файла

--- lang_frequency.py
+++ lang_frequency.py
@@ -3,14 +3,21 @@ import string
 
 
 def load_data(filepath):
-    with open(filepath) as file:
-        words = file.read().lower().split()
-        words = [word.rstrip(string.punctuation) for word in words if len(word.rstrip(string.punctuation)) > 0]
-    return words
+    try:
+        with open(filepath) as file:
+            words = file.read().lower().split()
+            words = [word.rstrip(string.punctuation) for word in words if len(word.rstrip(string.punctuation)) > 0]
+        return words
+    except FileNotFoundError:
+        print("Такого файла не существует")
+        return None
 
 
 def get_most_frequent_words(words):
-    print(*sorted(set(words), key=words.count, reverse=True)[:10], sep='\n')
+    if not words:
+        pass
+    else:
+        print(*sorted(set(words), key=words.count, reverse=True)[:10], sep='\n')
 
 
 if __name__ == '__main__':
