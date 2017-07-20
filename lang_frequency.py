bb31bb09c6ea9bf2bd954db03221e0310a730024
denis5417 <denisz.pol@yandex.ru>
2017-07-20 10:23:07 +0300

    load_data разбита на 2 функции, pass заменен на return, убрано магическое число

--- lang_frequency.py
+++ lang_frequency.py
@@ -6,23 +6,29 @@ import string
 def load_data(filepath):
     try:
         with open(filepath) as file:
-            words = file.read().lower().split()
-            words = [word.rstrip(string.punctuation) for word in words if len(word.rstrip(string.punctuation)) > 0]
-        return words
+            text = file.read()
+        return get_words_from_text(text)
     except FileNotFoundError:
         print("Такого файла не существует")
         return None
 
 
+def get_words_from_text(text):
+    words = text.lower().split()
+    words = [word.rstrip(string.punctuation) for word in words if len(word.rstrip(string.punctuation))]
+    return words
+
+
 def get_most_frequent_words(words):
     if not words:
-        pass
+        return None
     else:
-        print("\n".join(pair[0] for pair in Counter(words).most_common(10)))
+        output_words_quantity = 10
+        print("\n".join(pair[0] for pair in Counter(words).most_common(output_words_quantity)))
 
 
 if __name__ == '__main__':
-    try:
+    if len(sys.argv) > 1:
         get_most_frequent_words(load_data(sys.argv[1]))
-    except IndexError:
+    else:
         print("Введите название файла")
